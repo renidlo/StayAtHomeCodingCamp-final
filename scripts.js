1959e44dffc2388b2556a925295216a6941a04da
renidlo <63668776+renidlo@users.noreply.github.com>
2020-04-22 18:05:34 +0200

    now you can use more images
    Now you can use more images than 8 - thumbnails pictures are changing when it is needed.
    4 images added - so 12 images total now. Images alphabetically sorted.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,27 @@
-var imagesData = [
+var sourceData = [
+  {
+    photo: "./images/athletics.jpg",
+    title: "Athletics",
+    description:
+      "Athletics refers to track and field sports such as running, the high jump, and the javelin.",
+  },
+  {
+    photo: "./images/baseball.jpg",
+    title: "Baseball",
+    description:
+      "Baseball is a game played by two teams of nine players. Each player from one team hits a ball with a bat and then tries to run around three bases and get to the home base before the other team can get the ball back.",
+  },
   {
     photo: "./images/basketball.jpg",
     title: "Basketball",
     description:
       "Basketball is a game in which two teams of five players each try to score goals by throwing a large ball through a circular net fixed to a metal ring at each end of the court.",
+  },
+  {
+    photo: "./images/darts.jpg",
+    title: "Darts",
+    description:
+      "Darts is a game in which darts are thrown at a target usually marked with concentric circles divided into segments and with a bull's- eye in the center.",
   },
   {
     photo: "./images/football.jpg",
@@ -29,6 +47,12 @@ var imagesData = [
     description: "When people ski, they move over snow or water on skis.",
   },
   {
+    photo: "./images/Snooker.jpg",
+    title: "Snooker",
+    description:
+      "Snooker is a game involving balls on a large table. The players use a long stick to hit a white ball, and score points by knocking coloured balls into the pockets at the sides of the table.",
+  },
+  {
     photo: "./images/swimming.jpg",
     title: "Swimming",
     description:
@@ -48,6 +72,27 @@ var imagesData = [
   },
 ];
 
+// number of images
+var numberOfImages = sourceData.length;
+
+var imagesData = [];
+// fill imagesData from sourceData with leftI as first index
+function fillImages(leftI) {
+  for (let i = 0; i < 8; i++) {
+    if (leftI + i > numberOfImages - 1) {
+      imagesData[i] = sourceData[leftI - numberOfImages + i];
+    } else {
+      imagesData[i] = sourceData[leftI + i];
+    }
+  }
+}
+
+// first left index of sourceData is 0
+var leftIndex = 0;
+
+// first images filling
+fillImages(0);
+
 // first picture as default
 var highlightedThumbnail = 0;
 displayPhoto(imagesData[highlightedThumbnail]);
@@ -59,7 +104,13 @@ $(".arrow-l").click(function () {
 
   highlightedThumbnail = highlightedThumbnail - 1;
   if (highlightedThumbnail === -1) {
-    highlightedThumbnail = 7;
+    leftIndex = leftIndex - 1;
+    if (leftIndex === -1) {
+      leftIndex = numberOfImages - 1;
+    }
+    highlightedThumbnail = 0;
+    fillImages(leftIndex);
+    displayAllThumbnails();
   }
   displayPhoto(imagesData[highlightedThumbnail]);
   var clickedBox = `[data-index="` + highlightedThumbnail + `"]`;
@@ -73,7 +124,13 @@ $(".arrow-r").click(function () {
 
   highlightedThumbnail = highlightedThumbnail + 1;
   if (highlightedThumbnail === 8) {
-    highlightedThumbnail = 0;
+    leftIndex = leftIndex + 1;
+    if (leftIndex === numberOfImages) {
+      leftIndex = 0;
+    }
+    highlightedThumbnail = 7;
+    fillImages(leftIndex);
+    displayAllThumbnails();
   }
   displayPhoto(imagesData[highlightedThumbnail]);
   var clickedBox = `[data-index="` + highlightedThumbnail + `"]`;
@@ -88,10 +145,17 @@ function displayPhoto(element) {
   $("#photo-description").text(element.description);
 }
 
-// function for displaying thumbnail
+// function for displaying one thumbnail
 function displayThumbnail(element, ind) {
   var xxx = `[data-index="` + ind + `"]`;
   $(xxx).css("background-image", `url(` + element.photo + `)`);
+}
+
+// function for displaying all thumbnails
+function displayAllThumbnails() {
+  for (let j = 0; j < 8; j++) {
+    displayThumbnail(imagesData[j], j);
+  }
 }
 
 // create thumbnails
@@ -131,7 +195,13 @@ $(".small-arrow-l").click((event) => {
   $(previousBox).css("border", "3px solid gray");
   highlightedThumbnail = highlightedThumbnail - 1;
   if (highlightedThumbnail === -1) {
-    highlightedThumbnail = 7;
+    leftIndex = leftIndex - 1;
+    if (leftIndex === -1) {
+      leftIndex = numberOfImages - 1;
+    }
+    highlightedThumbnail = 0;
+    fillImages(leftIndex);
+    displayAllThumbnails();
   }
   displayPhoto(imagesData[highlightedThumbnail]);
   var clickedBox = `[data-index="` + highlightedThumbnail + `"]`;
@@ -144,7 +214,13 @@ $(".small-arrow-r").click((event) => {
   $(previousBox).css("border", "3px solid gray");
   highlightedThumbnail = highlightedThumbnail + 1;
   if (highlightedThumbnail === 8) {
-    highlightedThumbnail = 0;
+    leftIndex = leftIndex + 1;
+    if (leftIndex === numberOfImages) {
+      leftIndex = 0;
+    }
+    highlightedThumbnail = 7;
+    fillImages(leftIndex);
+    displayAllThumbnails();
   }
   displayPhoto(imagesData[highlightedThumbnail]);
   var clickedBox = `[data-index="` + highlightedThumbnail + `"]`;
